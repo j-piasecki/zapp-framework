f068566e0c481b9837517528e86ace356a4c4557
Jakub Piasecki <jakubpiasecki67@gmail.com>
2022-08-27 12:54:47 +0200

    Queue update only when the value changes

--- @zapp/core/src/working_tree/effects/RememberedMutableValue.ts
+++ @zapp/core/src/working_tree/effects/RememberedMutableValue.ts
@@ -7,16 +7,17 @@ export class RememberedMutableValue<T> extends RememberedValue<T> {
     return this._value
   }
 
-  public set value(val: T) {
-    this._value = val
+  public set value(newValue: T) {
+    const oldValue = this._value
+    this._value = newValue
 
     if (this.context.isDropped) {
       const newNode = WorkingTree.root.getNodeFromPath(this.context.path.concat(this.context.id))
       if (newNode !== null) {
         const remembered = (newNode as RememberNode).remembered as RememberedMutableValue<T>
-        remembered.value = val
+        remembered.value = newValue
       }
-    } else {
+    } else if (oldValue !== newValue) {
       WorkingTree.queueUpdate(this.context.parent!)
     }
   }
