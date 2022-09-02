f79bd6a36feadf19feb50874f3035fd14f5220c8
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-09-02 13:23:49 +0200

    Add comments

--- @zapp/core/src/working_tree/views/Custom.ts
+++ @zapp/core/src/working_tree/views/Custom.ts
@@ -6,10 +6,32 @@ import { ConfigBuilder } from '../props/Config.js'
 import { RenderNode } from '../../renderer/Renderer.js'
 
 export interface CustomViewProps extends Record<string, unknown> {
+  /**
+   * invoked when the node is dropped from the tree
+   */
   dropHandler?: () => void
+
+  /**
+   * invoked when the view enters the view hierarchy, when implemented itmust return a
+   * reference to the created view
+   */
   createView?: (config: RenderNode) => unknown
+
+  /**
+   * invoked after the ViewManager finishes setting up the newly created view, it may
+   * be used to overwrite them
+   */
   overrideViewProps?: (config: RenderNode) => void
+
+  /**
+   * invoked during update, receives previous and the next node
+   */
   updateView?: (previous: RenderNode, next: RenderNode) => void
+
+  /**
+   * invoked when the view leaves the view hierarchy, when implemented it must delete the created
+   * view (note that this method may be called without calling dropHandler to reorder views)
+   */
   deleteView?: (config: RenderNode) => void
 }
 
