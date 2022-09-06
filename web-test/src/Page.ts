cf4f96cfb415b09136d23b2949668ec855a83584
Jakub Piasecki <jakubpiasecki67@gmail.com>
2022-09-06 19:52:50 +0200

    Update web

--- web-test/src/Page.ts
+++ web-test/src/Page.ts
@@ -1,8 +1,8 @@
-import { Config, Row, Stack, StackConfig, Screen, StackAlignment, RowConfig } from '@zapp/core'
+import { Config, Row, Stack, StackConfig, SimpleScreen, StackAlignment, RowConfig } from '@zapp/core'
 import { NavBar, RouteInfo } from './NavBar'
 
 export function Page(routes: RouteInfo[], content: () => void) {
-  Screen(Config('screen'), () => {
+  SimpleScreen(Config('screen'), () => {
     Row(RowConfig('navbar-container').fillSize().background(0x000000), () => {
       NavBar(routes)
       Stack(StackConfig('content').weight(1).fillHeight().alignment(StackAlignment.Center), content)
