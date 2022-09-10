555cd0ad71c07d1bec28257e3774c2dd8dbe0cd6
Jakub Piasecki <jakubpiasecki67@gmail.com>
2022-09-10 21:08:26 +0200

    Implement `launch for result` on watch navigator

--- @zapp/watch/src/Application.ts
+++ @zapp/watch/src/Application.ts
@@ -1,15 +1,18 @@
 import { ApplicationConfig } from '@zapp/core'
+import { NavigatorData } from './Navigator'
 
 export function Application(config: ApplicationConfig) {
+  const navigatorData: NavigatorData = {
+    currentPage: 'index',
+    navStack: [],
+    savedStates: [],
+    registeredCallbacks: [],
+    shouldRestore: false,
+  }
+
   App({
     globalData: {
-      _navigator: {
-        currentPage: 'index',
-        stack: [],
-        savedStates: [],
-        registeredCallbacks: [],
-        shouldRestore: false,
-      },
+      _navigator: navigatorData,
     },
     onCreate(params: string) {
       config.onInit?.(params)
--- @zapp/watch/src/Navigator.ts
+++ @zapp/watch/src/Navigator.ts
@@ -1,36 +1,73 @@
-import { NavigatorInterface, RegisteredCallback, WorkingTree } from '@zapp/core'
+import { NavigatorInterface, RegisteredCallback, WorkingTree, SavedTreeState } from '@zapp/core'
+
+export interface NavigatorData {
+  currentPage: string
+  navStack: string[]
+  savedStates: SavedTreeState[]
+  registeredCallbacks: RegisteredCallback[]
+  shouldRestore: boolean
+}
 
 export class Navigator implements NavigatorInterface {
   get currentPage(): string {
     return getApp()._options.globalData._navigator.currentPage
   }
 
-  public navigate(page: string, params: Record<string, unknown>) {
-    const navigatorData = getApp()._options.globalData._navigator
-    navigatorData.stack.push(navigatorData.currentPage)
-    navigatorData.currentPage = page
-    navigatorData.savedStates.push(WorkingTree.saveState())
+  private get data(): NavigatorData {
+    return getApp()._options.globalData._navigator
+  }
 
-    hmApp.gotoPage({ url: page, param: JSON.stringify(params) })
+  public navigate(page: string, params?: Record<string, unknown>) {
+    this.data.navStack.push(this.data.currentPage)
+    this.data.currentPage = page
+    this.data.savedStates.push(WorkingTree.saveState())
+
+    hmApp.gotoPage({ url: page, param: JSON.stringify(params ?? {}) })
   }
 
   public goBack() {
-    const navigatorData = getApp()._options.globalData._navigator
-    navigatorData.currentPage = navigatorData.stack.pop()
-    navigatorData.shouldRestore = true
+    this.data.currentPage = this.data.navStack.pop()!
+    this.data.shouldRestore = true
 
     hmApp.goBack()
   }
 
-  registerResultCallback(page: string, path: string[]): void {
-    throw new Error('Method not implemented.')
+  public registerResultCallback(page: string, path: string[]): void {
+    this.data.registeredCallbacks.push({
+      targetPage: page,
+      callbackPath: path,
+      ready: false,
+    })
   }
 
-  tryPoppingLauncherResult(page: string, path: string[]): RegisteredCallback | undefined {
-    throw new Error('Method not implemented.')
+  public tryPoppingLauncherResult(page: string, path: string[]): RegisteredCallback | undefined {
+    if (this.data.registeredCallbacks.length > 0) {
+      const top = this.data.registeredCallbacks[this.data.registeredCallbacks.length - 1]
+
+      if (top.ready && page === top.targetPage && top.callbackPath.length === path.length) {
+        for (let i = 0; i < path.length; i++) {
+          if (path[i] !== top.callbackPath[i]) {
+            return undefined
+          }
+        }
+
+        return this.data.registeredCallbacks.pop()
+      }
+    }
+
+    return undefined
   }
 
-  finishWithResult(params: Record<string, unknown>): void {
-    throw new Error('Method not implemented.')
+  public finishWithResult(params: Record<string, unknown>): void {
+    if (this.data.registeredCallbacks.length > 0) {
+      const top = this.data.registeredCallbacks[this.data.registeredCallbacks.length - 1]
+
+      if (top.targetPage === this.currentPage) {
+        top.ready = true
+        top.result = params
+      }
+    }
+
+    this.goBack()
   }
 }
