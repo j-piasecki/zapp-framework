32124e70f95722fe237d72417d9619fe33ca4617
Jakub Piasecki <jakubpiasecki67@gmail.com>
2022-09-22 23:38:52 +0200

    Update ActivityIndicator

--- @zapp/ui/src/ActivityIndicator.ts
+++ @zapp/ui/src/ActivityIndicator.ts
@@ -1,33 +1,16 @@
-import { Arc, ArcConfigBuilder, Config, remember, sideEffect, Stack, withTiming } from '@zapp/core'
+import { Arc, ArcConfigBuilder, Config, remember, sideEffect, Stack, withRepeat, withTiming } from '@zapp/core'
 import { Theme } from './Theme.js'
 
 export function ActivityIndicator(config: ArcConfigBuilder) {
   const rawConfig = config.build()
   Stack(Config(`${rawConfig.id}#wrapper`), () => {
     const angle = remember(-90)
-    const size = remember(10)
+    const size = remember(30)
 
-    function animateCycle() {
-      angle.value = -90
-      angle.value = withTiming(270, {
-        duration: 1000,
-        onEnd: (completed) => {
-          if (completed) {
-            animateCycle()
-          }
-        },
-      })
-      size.value = withTiming(120, {
-        duration: 500,
-        onEnd: (completed) => {
-          if (completed) {
-            size.value = withTiming(10, { duration: 500 })
-          }
-        },
-      })
-    }
-
-    sideEffect(animateCycle)
+    sideEffect(() => {
+      angle.value = withRepeat(withTiming(270, { duration: 1000 }))
+      size.value = withRepeat(withTiming(120, { duration: 500 }), { reverse: true })
+    })
 
     if (rawConfig.borderColor === undefined) {
       config.color(Theme.primary)
