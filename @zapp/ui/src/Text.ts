c028dd6135c4d7a7cf7a462fbe8ed4ab78cc8236
Jakub Piasecki <jakubpiasecki67@gmail.com>
2022-09-25 19:32:36 +0200

    Update text

--- @zapp/ui/src/Text.ts
+++ @zapp/ui/src/Text.ts
@@ -1,7 +1,10 @@
 import { TextConfigBuilder, BareText } from '@zapp/core'
+import { Theme } from './Theme.js'
 
 const nextColors: number[] = []
 const nextFontSizes: number[] = []
+
+const DEFAULT_TEXT_SIZE = 32
 
 export function Text(config: TextConfigBuilder, text: string) {
   const rawConfig = config.build()
@@ -14,6 +17,13 @@ export function Text(config: TextConfigBuilder, text: string) {
   const nextFontSize = nextFontSizes[nextFontSizes.length - 1]
   if (rawConfig.textSize === undefined && nextFontSize !== null) {
     config.textSize(nextFontSize)
+  }
+
+  if (rawConfig.textSize === undefined) {
+    config.textSize(DEFAULT_TEXT_SIZE)
+  }
+  if (rawConfig.textColor === undefined) {
+    config.textColor(Theme.onBackground)
   }
 
   BareText(config, text)
